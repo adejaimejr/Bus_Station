416ac715970cc064ddfca4583c320802badf6054
LAPTOP-AJR84HS4\Julio <jcfsouza@yahoo.com.br>
2018-09-12 04:14:46 -0300

    Julio - Implementação da edição das viagens - Adiciona motorista, onibus, rota e tarifa - apresenta outros campos relacionados no grid - na alteração mostra os campos relacionados para alterar - quando adiciona uma nova viagem, é gerado registros nas tabelas:  tbviagens_motorista  tbviagens_onibus  tbviagens_rotas  tbviagens_tarifas
    * estes registros são copiados das tabelas originais, tbrotas, tbtarifas, tbonibus, tbmotorista
    e após atualiza os novos ids na tabela tbviagem.
    Desta forma quando se altera os valores da viagem, não altera os valores das tabelas originais (modelos)
    
    *obs: Precisa adicionar os campos na edição e no grid.

--- dataTables/viagem/php/table.tbviagem.php
+++ dataTables/viagem/php/table.tbviagem.php
@@ -33,214 +33,176 @@ use
 // performance.
 $db->sql( "CREATE TABLE IF NOT EXISTS `tbviagem` (
 	`id` int(10) NOT NULL auto_increment,
-	`data_saida` date,
-	`data_chegada` date,
-	`hora_chegada` time,
-	`hora_saida` time,
-	`destino_rotas` tinyint(4),
-	`origem_rotas` tinyint(4),
-	`normal_tarifa` decimal(9,2),
-	`promocional_tarifa` decimal(9,2),
-	`meiapassagem_tarifa` decimal(9,2),
-	`pedagio_tarifa` decimal(9,2),
-	`seguro_tarifa` decimal(9,2),
-	`poltronas_01` tinyint(4),
-	`poltronas_02` tinyint(4),
-	`poltronas_03` tinyint(4),
-	`poltronas_04` tinyint(4),
-	`poltronas_05` tinyint(4),
-	`poltronas_06` tinyint(4),
-	`poltronas_07` tinyint(4),
-	`poltronas_08` tinyint(4),
-	`poltronas_09` tinyint(4),
-	`poltronas_10` tinyint(4),
-	`poltronas_11` tinyint(4),
-	`poltronas_12` tinyint(4),
-	`poltronas_13` tinyint(4),
-	`poltronas_14` tinyint(4),
-	`poltronas_15` tinyint(4),
-	`poltronas_16` tinyint(4),
-	`poltronas_17` tinyint(4),
-	`poltronas_18` tinyint(4),
-	`poltronas_19` tinyint(4),
-	`poltronas_20` tinyint(4),
-	`poltronas_21` tinyint(4),
-	`poltronas_22` tinyint(4),
-	`poltronas_23` tinyint(4),
-	`poltronas_24` tinyint(4),
-	`poltronas_25` tinyint(4),
-	`poltronas_26` tinyint(4),
-	`poltronas_27` tinyint(4),
-	`poltronas_28` tinyint(4),
-	`poltronas_29` tinyint(4),
-	`poltronas_30` tinyint(4),
-	`poltronas_31` tinyint(4),
-	`poltronas_32` tinyint(4),
-	`poltronas_33` tinyint(4),
-	`poltronas_34` tinyint(4),
-	`poltronas_35` tinyint(4),
-	`poltronas_36` tinyint(4),
-	`poltronas_37` tinyint(4),
-	`poltronas_38` tinyint(4),
-	`poltronas_39` tinyint(4),
-	`poltronas_40` tinyint(4),
-	`poltronas_41` tinyint(4),
-	`poltronas_42` tinyint(4),
-	`poltronas_43` tinyint(4),
-	`poltronas_44` tinyint(4),
-	`poltronas_45` tinyint(4),
-	`poltronas_46` tinyint(4),
-	`poltronas_47` tinyint(4),
-	`poltronas_48` tinyint(4),
-	`poltronas_49` tinyint(4),
-	`poltronas_50` tinyint(4),
-	`poltronas_51` tinyint(4),
-	`poltronas_52` tinyint(4),
-	`poltronas_53` tinyint(4),
-	`poltronas_54` tinyint(4),
-	`poltronas_55` tinyint(4),
-	`poltronas_56` tinyint(4),
+	`rota` int(10) NOT NULL,
+	`onibus` int(10) NOT NULL,
+	`tarifa` int(10) NOT NULL,
+	`motorista` int(10) NOT NULL,
 	PRIMARY KEY( `id` )
 );" );
 
 // Build our Editor instance and process the data coming from _POST
 Editor::inst( $db, 'tbviagem', 'id' )
 	->fields(
-		Field::inst( 'data_saida' )
-			->validator( Validate::dateFormat( 'D, j M y' ) )
-			->getFormatter( Format::dateSqlToFormat( 'D, j M y' ) )
-			->setFormatter( Format::dateFormatToSql( 'D, j M y' ) ),
-		Field::inst( 'data_chegada' )
-			->validator( Validate::dateFormat( 'D, j M y' ) )
-			->getFormatter( Format::dateSqlToFormat( 'D, j M y' ) )
-			->setFormatter( Format::dateFormatToSql( 'D, j M y' ) ),
-		Field::inst( 'hora_chegada' )
-			->validator( Validate::dateFormat( 'H:i' ) )
-			->getFormatter( Format::datetime( 'H:i:s', 'H:i' ) )
-			->setFormatter( Format::datetime( 'H:i', 'H:i:s' ) ),
-		Field::inst( 'hora_saida' )
-			->validator( Validate::dateFormat( 'H:i' ) )
-			->getFormatter( Format::datetime( 'H:i:s', 'H:i' ) )
-			->setFormatter( Format::datetime( 'H:i', 'H:i:s' ) ),
-		Field::inst( 'destino_rotas' ),
-		Field::inst( 'origem_rotas' ),
-		Field::inst( 'normal_tarifa' ),
-		Field::inst( 'promocional_tarifa' ),
-		Field::inst( 'meiapassagem_tarifa' ),
-		Field::inst( 'pedagio_tarifa' ),
-		Field::inst( 'seguro_tarifa' ),
-		Field::inst( 'poltronas_01' )
-			->set( false ),
-		Field::inst( 'poltronas_02' )
-			->set( false ),
-		Field::inst( 'poltronas_03' )
-			->set( false ),
-		Field::inst( 'poltronas_04' )
-			->set( false ),
-		Field::inst( 'poltronas_05' )
-			->set( false ),
-		Field::inst( 'poltronas_06' )
-			->set( false ),
-		Field::inst( 'poltronas_07' )
-			->set( false ),
-		Field::inst( 'poltronas_08' )
-			->set( false ),
-		Field::inst( 'poltronas_09' )
-			->set( false ),
-		Field::inst( 'poltronas_10' )
-			->set( false ),
-		Field::inst( 'poltronas_11' )
-			->set( false ),
-		Field::inst( 'poltronas_12' )
-			->set( false ),
-		Field::inst( 'poltronas_13' )
-			->set( false ),
-		Field::inst( 'poltronas_14' )
-			->set( false ),
-		Field::inst( 'poltronas_15' )
-			->set( false ),
-		Field::inst( 'poltronas_16' )
-			->set( false ),
-		Field::inst( 'poltronas_17' )
-			->set( false ),
-		Field::inst( 'poltronas_18' )
-			->set( false ),
-		Field::inst( 'poltronas_19' )
-			->set( false ),
-		Field::inst( 'poltronas_20' )
-			->set( false ),
-		Field::inst( 'poltronas_21' )
-			->set( false ),
-		Field::inst( 'poltronas_22' )
-			->set( false ),
-		Field::inst( 'poltronas_23' )
-			->set( false ),
-		Field::inst( 'poltronas_24' )
-			->set( false ),
-		Field::inst( 'poltronas_25' )
-			->set( false ),
-		Field::inst( 'poltronas_26' )
-			->set( false ),
-		Field::inst( 'poltronas_27' )
-			->set( false ),
-		Field::inst( 'poltronas_28' )
-			->set( false ),
-		Field::inst( 'poltronas_29' )
-			->set( false ),
-		Field::inst( 'poltronas_30' )
-			->set( false ),
-		Field::inst( 'poltronas_31' )
-			->set( false ),
-		Field::inst( 'poltronas_32' )
-			->set( false ),
-		Field::inst( 'poltronas_33' )
-			->set( false ),
-		Field::inst( 'poltronas_34' )
-			->set( false ),
-		Field::inst( 'poltronas_35' )
-			->set( false ),
-		Field::inst( 'poltronas_36' )
-			->set( false ),
-		Field::inst( 'poltronas_37' )
-			->set( false ),
-		Field::inst( 'poltronas_38' )
-			->set( false ),
-		Field::inst( 'poltronas_39' )
-			->set( false ),
-		Field::inst( 'poltronas_40' )
-			->set( false ),
-		Field::inst( 'poltronas_41' )
-			->set( false ),
-		Field::inst( 'poltronas_42' )
-			->set( false ),
-		Field::inst( 'poltronas_43' )
-			->set( false ),
-		Field::inst( 'poltronas_44' )
-			->set( false ),
-		Field::inst( 'poltronas_45' )
-			->set( false ),
-		Field::inst( 'poltronas_46' )
-			->set( false ),
-		Field::inst( 'poltronas_47' )
-			->set( false ),
-		Field::inst( 'poltronas_48' )
-			->set( false ),
-		Field::inst( 'poltronas_49' )
-			->set( false ),
-		Field::inst( 'poltronas_50' )
-			->set( false ),
-		Field::inst( 'poltronas_51' )
-			->set( false ),
-		Field::inst( 'poltronas_52' )
-			->set( false ),
-		Field::inst( 'poltronas_53' )
-			->set( false ),
-		Field::inst( 'poltronas_54' )
-			->set( false ),
-		Field::inst( 'poltronas_55' )
-			->set( false ),
-		Field::inst( 'poltronas_56' )
-			->set( false )
+		Field::inst( 'tbviagem.rota' )
+			->options( Options::inst()			
+			->table( 'tbrotas b, location origem, location dest' )
+			->value( 'b.id' )
+			->label( ['b.id', 'b.origem', 'origem.id', 'origem.cidade', 'origem.uf', 'dest.id', 'dest.cidade', 'dest.uf', 'horariopartida', 'horariochegada' ])
+			->render( function ( $row ) {
+				return $row['horariopartida'].' - '.$row['origem.cidade'].','.$row['origem.uf'].'/'.$row['dest.cidade'].','.$row['dest.uf'];
+			} )
+			->where( function ($q) {				
+				$q->where( 'origem.id', 'b.origem', '=', false); // false => segundo parâmetro é coluna; true => é valor
+				$q->and_where( 'dest.id', 'b.destino', '=', false); // false => segundo parâmetro é coluna; true => é valor
+			})
+			)
+			->validator( Validate::dbValues() ),
+//		Field::inst( 'tbviagens.origem' ),
+		Field::inst( 'tbviagem.onibus' )
+			->options( Options::inst()			
+			->table( 'tbonibus o' )
+			->value( 'o.id' )
+			->label( ['o.marca' ])
+			)
+			->validator( Validate::dbValues() ),
+		Field::inst( 'tbviagem.tarifa' )
+			->options( Options::inst()			
+			->table( 'tbtarifas t' )
+			->value( 't.id' )
+			->label( ['t.nome' ])
+			)
+			->validator( Validate::dbValues() ),
+		Field::inst( 'tbviagem.motorista' )
+			->options( Options::inst()			
+			->table( 'tbmotorista m' )
+			->value( 'm.id' )
+			->label( ['m.nome' ])
+			)
+			->validator( Validate::dbValues() ),
+		Field::inst( 'tbviagens_onibus.marca' ),
+		Field::inst( 'tbviagens_motorista.nome' ),
+		Field::inst( 'tbviagens_rotas.horariopartida' ),
+		Field::inst( 'tbviagens_rotas.horariochegada' ),
+		Field::inst( 'tbviagens_tarifas.nome' ),
+		Field::inst( 'tbviagens_tarifas.normal' )		
 	)
+    ->on( 'postCreate', function ( $editor, $id, $values, $row ) use ($db) {
+		//logChange( $editor->db(), 'create', $id, $values );
+		//$editor->db->insert( "tbviagens_rotas", ['origem' => '1']);	
+		//[ 'myField' => $_POST['data']['addstock'] ] );
+			
+		$data = $_POST['data'];
+		$viagem = $data[0]['tbviagem'];
+		$rota = $viagem['rota'];
+		$onibus = $viagem['onibus'];
+		$tarifa = $viagem['tarifa'];
+		$motorista = $viagem['motorista'];				
+
+		// rota
+		$db->sql( "insert tbviagens_rotas " .
+			"(origem, uforigem, codorigem, destino, ufdestino, coddestino, distancia, horariopartida, horariochegada) " .
+			"select origem, uforigem, codorigem, destino, ufdestino, coddestino, distancia, horariopartida, horariochegada " .
+			"from tbrotas where tbrotas.id = ".$rota );
+
+		$rows = $db->sql("select max(id) as id from tbviagens_rotas")->fetchAll();
+
+		$newRota = $rows[0]["id"];
+
+		$db->sql("update tbviagem set rota = ".$newRota." where id = ".$id);
+
+
+		// onibus
+		$db->sql( "insert tbviagens_onibus " .
+			"(placa, classe, poltronas, anofabricacao, chassi, renavam, marca, modelo, vencimentoipva, quilometragem) " .
+			"select placa, classe, poltronas, anofabricacao, chassi, renavam, marca, modelo, vencimentoipva, quilometragem " .
+			"from tbonibus where tbonibus.id = ".$onibus );
+
+		$rows = $db->sql("select max(id) as id from tbviagens_onibus")->fetchAll();
+
+		$newRota = $rows[0]["id"];
+
+		$db->sql("update tbviagem set onibus = ".$newRota." where id = ".$id);
+	
+		// tarifas
+		$db->sql( "insert tbviagens_tarifas " .
+			"(nome, normal, promocional, meiapassagem, pedagio, seguro) " .
+			"select nome, normal, promocional, meiapassagem, pedagio, seguro " .
+			"from tbtarifas where tbtarifas.id = ".$tarifa );
+
+		$rows = $db->sql("select max(id) as id from tbviagens_tarifas")->fetchAll();
+
+		$newRota = $rows[0]["id"];
+
+		$db->sql("update tbviagem set tarifa = ".$newRota." where id = ".$id);
+	
+		// motorista
+		$db->sql( "insert tbviagens_motorista " .
+			"(nome, cpf, nascimento, email, telefone, cnh, validadecnh) " .
+			"select nome, cpf, nascimento, email, telefone, cnh, validadecnh " .
+			"from tbmotorista where tbmotorista.id = ".$motorista );		
+		
+		$rows = $db->sql("select max(id) as id from tbviagens_motorista")->fetchAll();
+
+		$newRota = $rows[0]["id"];
+
+		$db->sql("update tbviagem set motorista = ".$newRota." where id = ".$id);
+	
+    } )	
+    //->on( 'postCreate', function ( $editor, $values ) {
+	//	$db->sql( "insert tbviagens_rotas (origem) values(1)" );		
+		//$editor->db->sql( "SELECT 1" );		
+		//$sql = $db->sql( "UPDATE tblitem SET ItemPrice = '".$rspricelookup->getColumnVal("DefaultPrice")."' WHERE ItemID = '".$rowid."'");
+		//IsClient( $editor->db(), $id ??? );		
+    //} )	
+	/*->on('postCreate', function($editor,$id,$values,$row) {
+        //error_log('t.t.p: Created: id= ' . $id . ', catR= ' . $row['category'] . ', descR= ' . $row['description'] . ', catV= ' . $values['category'] . ', descV= ' . $values['description']);
+		//notify($id,$values['category'],$values['description']);
+		alert("ok");
+    } )	*/
+	//->leftJoin( 'location', 'location.id', '=', 'tbrotas.origem' )
+	->leftJoin( 'tbviagens_rotas', 'tbviagens_rotas.id', '=', 'tbviagem.rota' )				
+	->leftJoin( 'tbviagens_onibus', 'tbviagens_onibus.id', '=', 'tbviagem.onibus' )
+	->leftJoin( 'tbviagens_motorista', 'tbviagens_motorista.id', '=', 'tbviagem.motorista' )
+	->leftJoin( 'tbviagens_tarifas', 'tbviagens_tarifas.id', '=', 'tbviagem.tarifa' )
 	->process( $_POST )
 	->json();
+
+	//if ($_SERVER['REQUEST_METHOD'] === 'POST') {	
+	if(isset($_POST['action']) && $_POST['action'] == 'create'){
+		/*$data = $_POST['data'];
+		$viagem = $data[0]['tbviagem'];
+		$rota = $viagem['rota'];
+		$onibus = $viagem['onibus'];
+		$tarifa = $viagem['tarifa'];
+		$motorista = $viagem['motorista'];				
+
+		//$dataset = $db->sql("select id from tbviagem where rota = ".$rota." and onibus = ".$onibus." and tarifa = ".$tarifa." and motorista = ".$motorista);
+		$db->sql( "insert tbviagens_rotas " .
+			"(origem, uforigem, codorigem, destino, ufdestino, coddestino, distancia, horariopartida, horariochegada) " .
+			"select origem, uforigem, codorigem, destino, ufdestino, coddestino, distancia, horariopartida, horariochegada " .
+			"from tbrotas where tbrotas.id = ".$rota );
+
+		$rows = $db->sql("select max(id) as id from tbviagens_rotas")->fetchAll();
+
+		$newRota = $rows[0]["id"];
+
+		//$dataset = $db->sql("update tbviagem set rota = ".$newRota." where id = ".$viagem);
+
+		//$db->sql( "update tbviagens set rota = ".newRotaId." where id = ".$viagemId);
+
+		$db->sql( "insert tbviagens_onibus " .
+			"(placa, classe, poltronas, anofabricacao, chassi, renavam, marca, modelo, vencimentoipva, quilometragem) " .
+			"select placa, classe, poltronas, anofabricacao, chassi, renavam, marca, modelo, vencimentoipva, quilometragem " .
+			"from tbonibus where tbonibus.id = ".$onibus );
+
+		$db->sql( "insert tbviagens_tarifas " .
+			"(nome, normal, promocional, meiapassagem, pedagio, seguro) " .
+			"select nome, normal, promocional, meiapassagem, pedagio, seguro " .
+			"from tbtarifas where tbtarifas.id = ".$tarifa );
+
+		$db->sql( "insert tbviagens_motorista " .
+			"(nome, cpf, nascimento, email, telefone, cnh, validadecnh) " .
+			"select nome, cpf, nascimento, email, telefone, cnh, validadecnh " .
+			"from tbmotorista where tbmotorista.id = ".$motorista );*/
+
+	}
